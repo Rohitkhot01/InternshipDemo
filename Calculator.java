30f0973b36de2f7899c2d9cced7066f53a1066b3
Arjunbbam <arjunbam.0987@gmail.com>
2025-08-22 16:29:56 +0530

    commit-arjun1

--- Calculator.java
+++ Calculator.java
@@ -1,12 +1,12 @@
 
 import java.util.Scanner;
+
 public class Calculator {
 
-    public static void main(String []args)
-    {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num1=sc.nextInt();
-
+        int num1 = sc.nextInt();
+        System.out.println("this is from arjun");
     }
 
 }
